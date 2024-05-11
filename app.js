0c560f6a29c11580e471702f29fcc2637d802026
OPERS121 <lad.bar@mail.ru>
2024-05-11 17:49:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,34 @@ const DemoAppViewport = {
 	}
 };
 
+enemy = []; 
+enemy.push({x:0,y:0});
+function game(){
+  for(i in enemy){
+    ctx.fillRect(enemy[i].x,enemy[i].y,32,32);
+  }
+}
+
+setInterval(game,1);
+
+const imageList = [
+	'img/background.png',
+	'img/background1.png',
+  ]
+  const img = document.querySelector('.img')
+  const next = document.getElementById('filtertrigger')
+  var numImage = 0
+  img.src = imageList[numImage]
+  
+  next.addEventListener('click', () => {
+	numImage++
+	if (numImage >= imageList.length) {
+	  numImage = 0
+	}
+	console.log(numImage >= imageList.length)
+	console.log(numImage)
+	img.src = imageList[numImage]
+  })
 
 let usercard = document.getElementById("usercard");
 
