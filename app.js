28a2d8834dcfa212a7978e9410ce11332bd4b661
OPERS121 <lad.bar@mail.ru>
2024-05-12 03:27:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
+
+const DemoAppViewport = {
+	init() {
+		DemoApp.init();
+		Telegram.WebApp.onEvent('viewportChanged', DemoAppViewport.setData);
+		DemoAppViewport.setData();
+	},
+	setData() {
+		document.querySelector('.viewport-border').setAttribute('text', window.innerWidth + ' x ' + round(Telegram.WebApp.viewportHeight, 2))
+		document.querySelector('.viewport-stable_border').setAttribute('text', window.innerWidth + ' x ' + round(Telegram.WebApp.viewportStableHeight, 2) +
+			' | is_expanded: ' + (Telegram.WebApp.isExpanded ? 'true' : 'false'));
+	}
+};
+
+
+//button.onclick = function () {
+//    this.btnClick = (this.btnClick || 0) + 1;
+//    if (this.btnClick == 3) {
+//        this.disabled = true;
+//    }
+//};
+
+let circle = document.getElementById('circle');
+let btnenter = document.getElementById('btnenter');
+btnenter.addEventListener('click', () => {
+	circle.style.display = 'block';
+	btnenter.style.display = 'none';
+  setTimeout(() => {
+    circle.style.display = 'none';
+  }, 3000);
+});
+
+var timerId = setTimeout(function tick() {
+	objectbtn.style.display = 'block';
+	timerId = setTimeout(tick, 5000);
+}, 5000);
+
+let objectbtn = document.getElementById('objectbtn');
+let collectobject = document.getElementById('collectobject');
+
+objectbtn.addEventListener('click', () => {
+	collectobject.style.display = 'block';
+  setTimeout(() => {
+    collectobject.style.display = 'none';
+	objectbtn.style.display = 'none';
+  }, 3000);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
