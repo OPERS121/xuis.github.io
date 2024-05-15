c82fb1f2a6622ed5eb5cf639e4e0a6076ba0b7c3
OPERS121 <lad.bar@mail.ru>
2024-05-16 02:55:16 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
+
+const DemoAppViewport = {
+	init() {
+		DemoApp.init();
+		Telegram.WebApp.onEvent('viewportChanged', DemoAppViewport.setData);
+		DemoAppViewport.setData();
+	},
+	setData() {
+		document.querySelector('.viewport-border').setAttribute('text', window.innerWidth + ' x ' + round(Telegram.WebApp.viewportHeight, 2))
+		document.querySelector('.viewport-stable_border').setAttribute('text', window.innerWidth + ' x ' + round(Telegram.WebApp.viewportStableHeight, 2) +
+			' | is_expanded: ' + (Telegram.WebApp.isExpanded ? 'true' : 'false'));
+	}
+};
+
+let circle = document.getElementById('circle');
+let btnenter = document.getElementById('btnenter');
+let container = document.getElementById('container');
+let background = document.getElementById('background');
+let background1 = document.getElementById('background1');
+btnenter.addEventListener('click', () => {
+	circle.style.display = 'block';
+	btnenter.style.display = 'none';
+  setTimeout(() => {
+    circle.style.display = 'none';
+	background.style.display = 'none';
+	container.style.display = 'block';
+	background1.style.display = 'block';
+	imggus.style.display = 'block';
+  }, 3000);
+});
+
+let objectbtn = document.getElementById('objectbtn');
+var timerId = setTimeout(function tick() {
+	objectbtn.style.display = 'block';
+	timerId = setTimeout(tick, 5000);
+}, 5000);
+
+let collectobject = document.getElementById('collectobject');
+objectbtn.addEventListener('click', () => {
+	collectobject.style.display = 'block';
+  setTimeout(() => {
+    collectobject.style.display = 'none';
+	objectbtn.style.display = 'none';
+  }, 3000);
+});
