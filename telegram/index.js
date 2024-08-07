d5578fc4daa208f1b3a7feab168c4793fef3b892
OPERS121 <lad.bar@mail.ru>
2024-08-08 01:57:31 +0300

    Add files via upload

---
+++ telegram/index.js
@@ -0,0 +1,20 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6493078996:AAECm7PoRykwOrfsCgNKFfDtNjQ3DzIJ1m8';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+	const chatId = msg.chat.id;
+	const resp = match[1];
+  
+	bot.sendMessage(chatId, resp);
+});
+
+
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+  
+	bot.sendMessage(chatId, 'Приветствую, ' + msg.from.first_name);
+});
+
